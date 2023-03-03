b5d227639c240b9f4b7993eec3294efdaab11047
Edin Žiga <92861453+EdinZiga@users.noreply.github.com>
2023-03-03 23:07:33 +0100

    Update to Dashboard
    Updates:
     - Put advertisement widget into separate file
     - Added constructor arguments to advertisement widget, it can now be fully reused
     - Added skeleton bottom navigation bar, add advertisement button, search button
     - Other minor quality of life changes

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,4 +1,39 @@
+/*
+Section by Edin Žiga
+Unique signature, totally not copied from random website
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀
+⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀ ⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀
+⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀
+⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀
+⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀
+⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀
+⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀
+⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀
+⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀
+⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀
+⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀ ⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀
+*/
+//Import statements
 import 'package:flutter/material.dart';
+import 'advertisementWidget.dart';
+import 'package:easy_search_bar/easy_search_bar.dart';
+
+
+generateAds(){
+  List<Widget> genAds = [];
+
+
+
+}
+
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -7,48 +42,73 @@ class Dashboard extends StatefulWidget {
   State<Dashboard> createState() => _DashboardState();
 }
 
+//Main part, navigation and all
 class _DashboardState extends State<Dashboard> {
-  @override
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static List<Widget> _widgetOptions = <Widget>[
-  SingleChildScrollView(
-    child: Column(
-    children: <Widget>[
-      Advertisement(),
-      Advertisement(),
-      Advertisement(),
-      Advertisement(),
-      Advertisement(),
-  ],
-  ),
-  ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Settings',
-      style: optionStyle,
-    ),
-  ];
+
+  String searchValue = '';
+  List<String> _suggestions = ['Ad1','Ad2','Ad3'];
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
-
+  @override
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  List<Widget> _widgetOptions = <Widget>[
+  SingleChildScrollView(
+    child: Column(
+    children: <Widget>[
+      Advertisement(title: 'Lorem ipsum dolor sit amet',
+          description: 'Tincidunt fermentum imperdiet. Fusce semper lectus id metus tincidunt, '
+              'sit amet pellentesque tellus consectetur.',
+          adImage: Image(image: AssetImage('assetsTesting/guySmiling.jpg'))),
+      Advertisement(title: 'Duis dignissim diam quis sodales efficitur.',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia nulla eget mollis sollicitudin. '
+              'Donec aliquam lorem et varius fringilla. ',
+          adImage: Image(image: AssetImage('assetsTesting/advertisementIconTest.jpg'))),
+      Advertisement(title: 'Etiam auctor semper arcu, vel consequat arcu feugiat at.',
+          description: 'Nulla mi nibh, dapibus sed bibendum vulputate, posuere sed massa. '
+              'Integer justo lorem, ultricies quis enim eu, lobortis elementum velit. ',
+          adImage: Image(image: AssetImage('assetsTesting/flutterLogo.jpg'))),
+      Advertisement(title: 'Sed porttitor justo quis enim bibendum auctor.',
+          description: 'Fusce vulputate dolor vitae sapien mollis, quis dapibus eros semper. '
+              'Donec sollicitudin metus odio, et pulvinar enim vestibulum non.',
+          adImage: Image(image: AssetImage('assetsTesting/flutterBird.png'))),
+    ],
+  ),
+  ),
+    Center(child: Text('Imagine you can see some messages')),
+    Center(child: Text('Imagine you can see a profile')),
+    Center(child: Text('Imagine you can see some settings')),
+  ];
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Currently Available Ads'),
+      appBar: EasySearchBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          foregroundColor: Colors.blue,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+            Expanded(
+            flex: 1,
+            child: Image(image: AssetImage('assetsTesting/flutterLogo.jpg'),
+              height: 35, width: 35,),
+          ),
+              Expanded(
+                flex: 10,
+                child: Center(
+                    child:
+                    Text('Imagine there is a title here')),
+              ),
+            ]
+      ),
+          onSearch: (value) => setState(() => searchValue = value),
+          suggestions: _suggestions
       ),
       body: Container(
           child: _widgetOptions.elementAt(_selectedIndex)
@@ -58,120 +118,51 @@ class _DashboardState extends State<Dashboard> {
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
-            backgroundColor: Colors.red,
+            backgroundColor: Colors.blue,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-            backgroundColor: Colors.green,
+            icon: Icon(Icons.message),
+            label: 'Messages',
+            backgroundColor: Colors.blue,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-            backgroundColor: Colors.purple,
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+            backgroundColor: Colors.blue,
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
             label: 'Settings',
-            backgroundColor: Colors.pink,
+            backgroundColor: Colors.blue,
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor: Colors.white,
         onTap: _onItemTapped,
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: onPressed,
+          onPressed: () => showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text('Can you not?'),
+              content: const Text('I really don\'t like you pressing buttons'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'Okay'),
+                  child: const Text('Okay'),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'Fine'),
+                  child: const Text('Fine'),
+                ),
+              ],
+            ),
+          ),
         child: Icon(
           Icons.add,
         ),
       ),
     );
-  }
-}
-
-class Advertisement extends StatefulWidget {
-  const Advertisement({Key? key}) : super(key: key);
-
-  @override
-  State<Advertisement> createState() => _AdvertisementState();
-}
-
-void onPressed() {
-  // TODO: implement onPressed
-}
-
-class _AdvertisementState extends State<Advertisement> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Container(
-            margin: EdgeInsets.fromLTRB(15, 8, 15, 8),
-            child: ClipRRect(
-            borderRadius: BorderRadius.circular(15.0),
-              child: ElevatedButton(
-                onPressed: onPressed,
-                child: Column(
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                       Padding(
-                         padding: EdgeInsets.fromLTRB(0, 13, 10, 10),
-                         child: ClipRRect(
-                           borderRadius: BorderRadius.circular(20.0),
-                           child: Container(
-                             height: 55.0,
-                             width: 55.0,
-                             child: Image(
-                                 image:AssetImage('assetsTesting/guySmiling.jpg'),
-                             ),
-                           ),
-                         ),
-                       ),
-                        Flexible(
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(0, 13, 10, 10),
-                            child: Text(
-                                'Lorem ipsum dolor sit amet',
-                            textAlign: TextAlign.start,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 23,
-                            ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Flexible(
-                            child: Padding(
-                              padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                              child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
-                                  'Fusce porta, metus at vehicula blandit, dui velit maximus lorem, '
-                                  'at dictum nulla felis id est. Phasellus non justo ac sem '
-                                  'lacinia suscipit.',
-                              textAlign: TextAlign.justify,
-                              style: TextStyle(
-                              ),),
-                            )
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-
   }
 }
 
