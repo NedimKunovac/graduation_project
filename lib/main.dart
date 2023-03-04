ff3d4162c4aff10a3c2604b1285146f011a96d47
Edin Žiga <92861453+EdinZiga@users.noreply.github.com>
2023-03-04 17:02:41 +0100

    Connected to Firebase/Firestore
    As title implies, that was done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dashboard.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MaterialApp(
     home:MyApp(),
   ));
@@ -12,7 +17,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Dashboard();
   }
 }
 
