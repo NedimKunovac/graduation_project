d7d6312d35c1984b704db1f69fbbe73ba4156693
Edin Žiga <92861453+EdinZiga@users.noreply.github.com>
2023-03-05 22:37:53 +0100

    Firebase auth rough implementation
    Testing with firebase auth, rough implementation, unfinished, not related to actual project

---
+++ lib/fbTest/fbLoginTestWidget.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:graduation_project/main.dart';
+
+class fbLoginTestWidget extends StatefulWidget {
+  final VoidCallback onClickedSignUp;
+
+  const fbLoginTestWidget({
+    Key? key,
+    navigatorKey,
+    required this.onClickedSignUp,
+}): super(key: key);
+
+
+  @override
+  State<fbLoginTestWidget> createState() => _fbLoginTestWidgetState();
+}
+
+class _fbLoginTestWidgetState extends State<fbLoginTestWidget> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void dispose(){
+    emailController.dispose();
+    passwordController.dispose();
+
+    super.dispose();
+  }
+
+  Future signIn() async{
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => Center(child: CircularProgressIndicator())
+    );
+
+    try{
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+
+   navigatorKey.currentState!.popUntil((route) => route.isFirst);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 40),
+          TextField(
+            controller: emailController,
+            cursorColor: Colors.white,
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(labelText: 'Email'),
+          ),
+          SizedBox(height: 4),
+          TextField(
+            controller: passwordController,
+            cursorColor: Colors.white,
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(labelText: 'Password'),
+            obscureText: true,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton.icon(
+            icon:Icon(Icons.lock_open, size: 32,),
+            label: Text(
+              'Sign In',
+              style: TextStyle(fontSize: 24),
+            ),
+            onPressed: signIn,
+          ),
+          SizedBox(height: 10),
+          RichText(
+              text: TextSpan(
+                text: 'No account?',
+                children: [
+                  TextSpan(
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = widget.onClickedSignUp,
+                    text: ' Sign Up',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                    )
+                  )
+                ]
+              ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'signorlog.dart';
+import 'fbTest/fbLoginTest.dart';
 
+
+final navigatorKey = GlobalKey<NavigatorState>();
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
   runApp(MaterialApp(
+    navigatorKey: navigatorKey,
     home:MyApp(),
   ));
 }
@@ -16,7 +20,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SignOrLog();
+    return TestSignupLogin(navigatorKey: navigatorKey);
   }
 }
 
