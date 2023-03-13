e92a7c7f06dc55fae8e6b4f47054c17a0d66b6ae
NedimKunovac <kunovac.nedim@outlook.com>
2023-03-13 02:52:59 +0100

    Slight fixes

--- lib/advertisementWidget.dart
+++ lib/advertisementWidget.dart
@@ -1,3 +1,7 @@
+/*
+This section is a template for the advertisements on the dashboard
+It requires three parameters, a title, a short description and a image
+*/
 //Import statements
 import 'package:flutter/material.dart';
 import 'package:graduation_project/advertisementDetailed.dart';
