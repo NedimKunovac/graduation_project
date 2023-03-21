769523078fc82f8eaecc09dd8de2174bd20edfed
Qo24 <redzepovicmirza@gmail.com>
2023-03-21 10:43:22 +0100

    Revert "..."
    This reverts commit 6c5cb3db38958c971e807f2058f2fe340a1c8ae2.

--- lib/advertisementDetailed.dart
+++ lib/advertisementDetailed.dart
@@ -4,7 +4,7 @@ class AdvertisementDetailed extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           elevation: 0,
@@ -19,7 +19,7 @@ class AdvertisementDetailed extends StatelessWidget {
                 color: Colors.black,
               )),
         ),
-        body: SingleChildScrollView(child:Column(
+        body: Column(
           children: [
             Container(
               height: MediaQuery.of(context).size.height * 0.3,
@@ -220,7 +220,7 @@ class AdvertisementDetailed extends StatelessWidget {
               ),
             ),
           ],
-        ),),
+        ),
         bottomNavigationBar: BottomAppBar(
           child: Container(
             height: 50.0,
@@ -244,4 +244,3 @@ class AdvertisementDetailed extends StatelessWidget {
     );
   }
 }
-
