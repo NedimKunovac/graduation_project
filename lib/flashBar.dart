d3f7abfb80e43d630a4b9fdea555c3e8c1a9c18d
Edin Žiga <92861453+EdinZiga@users.noreply.github.com>
2023-04-19 15:47:06 +0200

    Flashbar load on top change

--- lib/flashBar.dart
+++ lib/flashBar.dart
@@ -19,7 +19,7 @@ class flashBar {
           backgroundColor: Colors.red,
           controller: controller,
           behavior: flashStyle,
-          position: FlashPosition.bottom,
+          position: FlashPosition.top,
           boxShadows: kElevationToShadow[4],
           horizontalDismissDirection: HorizontalDismissDirection.horizontal,
           child: FlashBar(
@@ -48,7 +48,7 @@ class flashBar {
           backgroundColor: Colors.green,
           controller: controller,
           behavior: flashStyle,
-          position: FlashPosition.bottom,
+          position: FlashPosition.top,
           boxShadows: kElevationToShadow[4],
           horizontalDismissDirection: HorizontalDismissDirection.horizontal,
           child: FlashBar(
